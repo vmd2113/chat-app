0daed24144653603572f4c3012eb43e06c3e64b4
Duongw <vmd.2113@gmail.com>
2025-03-10 14:45:46 +0700

    initial project

---
+++ chat-app-be/src/main/java/com/duongw/chatapp/ChatAppApplication.java
@@ -0,0 +1,13 @@
+package com.duongw.chatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatAppApplication.class, args);
+    }
+
+}
